2d34d1aa84744b906a2fb751cf3e3125d6a7e562
Jason <jlong5795@gmail.com>
2020-05-06 17:35:05 -0500

    more progress

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,14 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -38,6 +28,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+jason = Player('Jason', 'outside')
 
 # Write a loop that:
 #
@@ -49,3 +40,6 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    
--- src/player.py
+++ src/player.py
@@ -2,12 +2,10 @@
 # currently.
 
 class Player:
-    def __init__(self, name, level, hp, current_room, items):
+    def __init__(self, name, current_room):
         self.name = name
-        self.level = level
-        self.hp = hp
         self.current_room = current_room
-        self.items = items
+        self.items = []
 
     def addItem(self, item):
         self.items.append(item)
@@ -19,7 +17,7 @@ class Player:
         self.hp = self.hp + change
 
     def __str__(self):
-        return f"My name is {self.name}. I am level {self.level}. My HP is {self.hp}. I am in {self.current_room}. I am holding {self.items} items."
+        return f"My name is {self.name}. I am in {self.current_room}. I am holding {self.items} items."
         
 
 
